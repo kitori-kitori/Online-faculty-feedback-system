afa4f7c7be76c72afe4caa0a998a8d3bb9971633
Sharifu Kitori <51135024+kitori-kitori@users.noreply.github.com>
2020-09-22 01:16:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -140,21 +140,21 @@ session_start();
         <div class="carousel-inner">
             <div class="item active">
                 
-                <div class="fill" style="background-image:url('images/feedback.jpg');"></div>
+                <div class="fill" style="background-image:url('#');"></div>
 				<div class="carousel-caption">
                     
                 </div>
             </div>
            
             <div class="item">
-                <div class="fill" style="background-image:url('images/feedback1.jpg');"></div>
+                <div class="fill" style="background-image:url('#');"></div>
                 <div class="carousel-caption">
                    
                 </div>
             </div>
 			
 			 <div class="item">
-                <div class="fill" style="background-image:url('images/feedback3.jpg');"></div>
+                <div class="fill" style="background-image:url('#');"></div>
                 <div class="carousel-caption">
                    
                 </div>
@@ -190,18 +190,8 @@ session_start();
 				
 			<div class="col-sm-10" style="margin-top:60px;margin-bottom:80px">
 				<h2>About Faculty feedback System</h2>
-				Student Feedback system for College  
-Here we have developed the a faculty feedback system, which is generally used in the college to rate the faculty based on the performance...Here we have 2 modules such as administrator, student.
-
-Administrator is the one who creates the student account by adding all student info and assigning the username and password. 
-Admin also checks the result once all students entered the feedback..
-We can start the development from the login page, where we have given the option to login as admin and student...Here since we have only one admin account, so no need to create the a database to store admin info...so the admin username is "admin" and password is "sandeep"...select admin in the radio button and login 
-
-You can perform all admin actions such as login to the account and check result..
-
-If you entered the student user and password and selected student option, then it will show all student information and let you enter the feedback based on the subject..
-
-Before we can look into the php code, you need to create a database called "feed" with two tables in it..one as student and another one as take
+				This is a simple online System which will simply allow university students to rate their lectures as well as faculties.
+				a student will be registered into the system,rate[0 - 5] make a comment,contacting their lectures
 	
 			</div>
 			
